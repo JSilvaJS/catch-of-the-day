bf949cf54fb143754239749f23ebfba044713276
Jonathan Silva <silva.jon86@gmail.com>
2017-06-06 19:42:22 -0400

    Created Components and added them to  App.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+//COMPONENT IMPORTS
+import App from './components/App';
+import Header from './components/Header';
+import Inventory from './components/Inventory';
+import StorePicker from './components/StorePicker';
+import Order from './components/Order';
+
+
+//CSS IMPORTS
+import './css/style.css';
+
+render(<App/>, document.querySelector('#main'));
