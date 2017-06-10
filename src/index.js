5e89d0845a5f4b4a92badeef855169191d944a0f
Jonathan Silva <silva.jon86@gmail.com>
2017-06-10 10:15:33 -0400

    Added React Router functionality to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 //COMPONENT IMPORTS
 import App from './components/App';
@@ -8,9 +9,26 @@ import Header from './components/Header';
 import Inventory from './components/Inventory';
 import StorePicker from './components/StorePicker';
 import Order from './components/Order';
+import NotFound from './components/NotFound';
 
 
 //CSS IMPORTS
 import './css/style.css';
 
-render(<App/>, document.querySelector('#main'));
+
+//Routing
+const Root = () => {
+	return (
+		<BrowserRouter>
+		 <div>
+		  <Match exactly pattern="/" component={StorePicker} />
+		  <Match exactly pattern="/store/:storeId" component={App} />
+		  <Miss component={NotFound} />
+		 </div>
+		</BrowserRouter>
+	)
+}
+
+
+
+render(<Root/>, document.querySelector('#main'));
