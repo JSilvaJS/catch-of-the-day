4aae18c8e1137e0b53b608623bb570cca95bde20
Jonathan Silva <silva.jon86@gmail.com>
2017-06-13 18:54:40 -0400

    Created base file and added the App's state to firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyC2EPvaHhxDT0VYOHMheY_vwmdGMXvX8wM",
+    authDomain: "catch-of-the-day-jon-silva.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jon-silva.firebaseio.com",
+});
+
+export default base;
