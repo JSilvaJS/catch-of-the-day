302f72739a48ea6a385c27d1e481963eb749eced
Jonathan Silva <silva.jon86@gmail.com>
2017-06-14 14:13:23 -0400

    Added Bi-directional Data flow with Live state changing

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,15 @@ class App extends React.Component {
 		super();
 
 		this.addFish = this.addFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		
 
 		this.state = {
-			//getinialState
+			//get initial State
 			fishes: {},
 			order: {}
 		};
@@ -58,6 +62,18 @@ class App extends React.Component {
 	  this.setState({ fishes });
 	}
 
+	updateFish(key, updateFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updateFish;
+		this.setState({ fishes });
+	}
+
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -70,6 +86,12 @@ class App extends React.Component {
 		//update or add the new number of fish ordered"available"
 		order[key] = order[key] + 1 || 1;
 		//update our state
+		this.setState({ order });
+	}
+
+	removeFromOrder(key) {
+		const order = {...this.state.order};
+		delete order[key];
 		this.setState({ order });
 	}
 
@@ -89,9 +111,16 @@ class App extends React.Component {
 				<Order 
 					fishes={this.state.fishes} 
 					order={this.state.order}
-					params={this.props.params} 
+					params={this.props.params}
+					removeFromOrder={this.removeFromOrder} 
 				/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+				<Inventory 
+					addFish={this.addFish}
+					removeFish={this.removeFish} 
+					loadSamples={this.loadSamples}
+					fishes={this.state.fishes}
+					updateFish={this.updateFish}
+				/>
 			</div>
 		)
 	}
